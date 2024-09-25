c684660f36e4df84252a61782165a4c644fd1ecf
JYOTI-MARKUN <jyotimarkun1997@gmail.com>
2024-09-25 17:39:11 +0530

    practice HOI

--- JAVASCRIPT/que/script.js
+++ JAVASCRIPT/que/script.js
@@ -17,4 +17,79 @@ console.log(str.repeat(2))
 console.log(("love").padEnd(5,"l"))
 console.log(("love").padStart(5,"l"))
 
+//HOI example
+const radius = [2,5,6,1,3]
+function area(radius){
+    return Math.PI * radius * radius
+}
+
+function perimeter (radius){
+    return 2 * Math.PI * radius
+}
+
+function calculate(radius,logic){
+    const output = []
+    for (let i =0 ;i<radius.length-1;i++){
+        output.push(logic(radius[i]))
+    }
+    return output;
+}
+
+console.log(calculate(radius,perimeter))
+
+// hoi examples
+// Write a function called multiplyBy that takes a number as an argument and returns a function that multiplies its input by that number.
+function multiplyBy(num){
+   return function multiply(num2){
+    return num * num2
+   }
+
+}
+
+const multipleByANumber = multiplyBy(3)
+
+console.log(multipleByANumber(5))
+
+
+// Create a higher-order function add that takes two numbers and returns a function that adds them together.
+
+function addTwoNumber(num1, num2){
+    return function addResult(){
+      return  num1+num2
+    }
+}
+
+const result = addTwoNumber(2,30)
+
+console.log(result())
+
+
+// Write a function makeCounter that returns a function that increments a counter each time it is called.
+
+function makeCounter(){
+  let counter = 0;
+    return function increaseCounter(){
+       return counter++
+    }
+}
+
+const currentCounterValue = makeCounter()
+
+console.log(currentCounterValue())
+console.log(currentCounterValue())
+console.log(currentCounterValue())
+console.log(currentCounterValue())
+console.log(currentCounterValue())
+
+//Use the map method to create a new array that contains the squares of an array of numbers.
+let array = [5,10,2,3,15,12,8]
+
+const newArray = array.map((num)=>num*num)
+
+console.log(newArray)
+
+//Use the filter method to return an array containing only the even numbers from an array
+const filteredArray = array.filter((num)=>num%2===0)
+
+console.log(filteredArray)
 
